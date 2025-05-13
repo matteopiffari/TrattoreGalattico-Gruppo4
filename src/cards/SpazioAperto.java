2c34e40ce0d3bebc2792d28db84b483ae38ea6b9
Burghix45064 <j.burgacahuana@studenti.unibg.it>
2025-05-13 14:30:25 +0200

    Update SpazioAperto.java

--- src/cards/SpazioAperto.java
+++ src/cards/SpazioAperto.java
@@ -1,6 +1,24 @@
 package cards;
 
 public class SpazioAperto extends Carta {
+    public SpazioAperto() {
+        super("Spazio Aperto", 0, 0, 0, 0, 0);
+    }
+
+    @Override
+    public String toString() {
+        return "Spazio Aperto";
+    }
+
+    @Override
+    public String getTipo() {
+        return "Spazio Aperto";
+    }
+
+    @Override
+    public String getDescrizione() {
+        return "Carta che rappresenta uno spazio aperto.";
+    }
 	
 
 }
