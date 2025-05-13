c77bd6bc3da7cdeba061e5ea74d41ece39ecd7dc
Matteo <mattipiffi@gmail.com>
2025-05-13 14:59:31 +0200

    Revert "Update Cards.java"
    This reverts commit ec1fd34d2863c42f4132745f4d3a90bfb543c335.

--- src/cards/Cards.java
+++ src/cards/Cards.java
@@ -8,7 +8,7 @@ public class Cards {
     private int valore4;
     private int valore5;
 
-    // costruttore di default 
+    // Constructor matching the required signature
     public Carta(String nome, int valore1, int valore2, int valore3, int valore4, int valore5) {
         this.nome = nome;
         this.valore1 = valore1;
@@ -18,29 +18,7 @@ public class Cards {
         this.valore5 = valore5;
     }
 
-    // metodi getter
-    public int getValore1() {
-        return valore1;
-    }
-    public int getValore2() {
-        return valore2;
-    }
-    public int getValore3() {
-        return valore3;
-    }
-    public int getValore4() {
-        return valore4;
-    }
-    public int getValore5() {
-        return valore5;
-    }
-    // metodo per ottenere il nome della carta
-    // questo metodo è stato aggiunto per ottenere il nome della carta
-    // in modo da poterlo utilizzare in altre classi
-    // come ad esempio nella classe Giocatore
-    // in modo da poterlo utilizzare per confrontare le carte
-    // e per stampare il nome della carta
-    // in modo da poterlo utilizzare per stampare il nome della carta   
+    // Getters and other methods (if needed)
     public String getNome() {
         return nome;
     }
